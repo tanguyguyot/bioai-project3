5910331522817ef814cb62bc1d2c75b032921406
tanguyguyot <guyot.tanguy1@gmail.com>
2025-04-16 19:04:13 +0200

    changes

--- table_creation.py
+++ table_creation.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
 
-def get_error_table(dataset, feature_columns, y) -> dict:
+def get_error_table(dataset, feature_columns, y, test_size=0.3, max_depth=3) -> dict:
     error_table = {}
     for i in tqdm(range(1, len(feature_columns) + 1)):
         for selected_columns in combinations(feature_columns, i):
@@ -17,10 +17,10 @@ def get_error_table(dataset, feature_columns, y) -> dict:
             # Get column indexes
             col_indexes = tuple([feature_columns.index(col) for col in selected_columns])
             # Split dataset into training set and test set
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=1)
             
             # Create a clf
-            clf = DecisionTreeClassifier(max_depth=3, random_state=1)
+            clf = DecisionTreeClassifier(max_depth=max_depth, random_state=1)
             
             # Train clf
             clf = clf.fit(X_train, y_train)
@@ -35,13 +35,14 @@ def get_error_table(dataset, feature_columns, y) -> dict:
     return error_table
 
 def get_lookup_table(error_table, penalty_factor):
-    lookup_table = {}
+    lookup_table = {"penalty_factor": penalty_factor}
     for key, value in error_table.items():
         lookup_table[key] = value - penalty_factor * len(key)
     return lookup_table
 
 def print_results(error_table, lookup_table, dataset_name):
     print("Results for dataset: ", dataset_name)
+    print(f"Penalty factor of {lookup_table['penalty_factor']}")
     print(f"Lookup wine table:{lookup_table}")
     print(f"Error table wine: {error_table}")
     print(f"Length : {len(lookup_table)}")
@@ -49,9 +50,28 @@ def print_results(error_table, lookup_table, dataset_name):
     # Get the 10 max values and key of error table
     max_values = sorted(error_table.values(), reverse=True)[:10]
     max_keys = sorted(error_table, key=error_table.get, reverse=True)[:10]
-    print(f"Max values glass: {max_values}")
-    print(f"Max keys glass: {max_keys}")
+    print(f"Max values glass (error table): {max_values}")
+    print(f"Max keys glass (error table): {max_keys}")
+    print(f"Max values glass (lookup table): {sorted(lookup_table.values(), reverse=True)[:10]}")
+    print(f"Max keys glass (lookup table): {sorted(lookup_table, key=lookup_table.get, reverse=True)[:10]}")
     
+def to_binary_representation(tuples, length):
+    binary_rep = "0" * length
+    for i in tuples:
+        print(tuples)
+        print(i)
+        binary_rep = binary_rep[:i] + "1" + binary_rep[i+1:]
+    return binary_rep
+
+def export_to_csv(table, name, column_amount):
+    # set table indexes to binary representation
+    binary_table = {}
+    for key, value in table.items():
+        binary_key = to_binary_representation(key, column_amount)
+        binary_table[binary_key] = value
+    df = pd.DataFrame(binary_table)
+    df.to_csv(f"{name}.csv", index=False)
+    print(f"Exported to {name}.csv")
     
 if __name__ == "__main__":
-    pass
+    print(to_binary_representation((1, 2), 5))
