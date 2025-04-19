fcc7d8012a666da6287535357cb6318bfa8337da
tanguyguyot <guyot.tanguy1@gmail.com>
2025-04-19 16:24:16 +0200

    feat: SGA

--- table_creation.py
+++ table_creation.py
@@ -8,6 +8,7 @@ from itertools import combinations
 from tqdm import tqdm
 from multiprocessing import Pool
 import matplotlib.pyplot as plt
+import numpy as np
 
 def get_error_table(dataset, feature_columns, y, test_size=0.3, max_depth=3) -> dict:
     error_table = {}
@@ -54,17 +55,18 @@ def print_results(error_table, lookup_table, dataset_name):
     print(f"Max values glass (lookup table): {sorted(lookup_table.values(), reverse=True)[:10]}")
     print(f"Max keys glass (lookup table): {sorted(lookup_table, key=lookup_table.get, reverse=True)[:10]}")
     
-def to_binary_representation(tuples, length):
+def to_binary_representation(tuples, length) -> str:
     binary_rep = "0" * length
     for i in tuples:
         binary_rep = binary_rep[:i] + "1" + binary_rep[i+1:]
     return binary_rep
 
-def get_complete_table(error_table, length_columns, penalty_factor):
+def get_complete_table(error_table, length_columns, penalty_factor) -> dict:
     complete_table = {}
     for key, value in error_table.items():
         complete_table[key] = [to_binary_representation(key, length_columns), value, value - penalty_factor * len(key),
         ]
+    complete_table["()"] = ["0" * length_columns, 0, 0]
     return complete_table
 
 def export_to_csv(complete_table, dataset_name):
@@ -78,6 +80,62 @@ def export_to_csv(complete_table, dataset_name):
     df.to_csv(f'{dataset_name}.csv', index=True)
     print(f"Complete table exported to {dataset_name}.csv")
     
+def visualization_2d(complete_table: dict, dataset_name: str) -> None:
+    # Add a column for convert binary representation to int
+    complete_table['Binary_to_int'] = complete_table['Binary representation'].apply(lambda x: int(x, 2))
+    X = complete_table['Binary_to_int']
+    y = complete_table['Lookup value']
+    ind=np.argsort(X)
+    
+    # Create a scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(X, y, alpha=0.5)
+    plt.plot(X[ind], y[ind], color='red', linewidth=0.5)
+    plt.title(f"Scatter plot of Lookup value vs Binary representation for {dataset_name}")
+    plt.xlabel("Binary representation (as int)")
+    plt.ylabel("Lookup value")
+    plt.grid()
+    plt.savefig(f"outputs/{dataset_name}_plot.png")
+    plt.show()
+    return
+    """
+def hinged_bitstring_map(complete_table:dict, dataset_name: str) -> None:
+    # SPlit bitstrings into 2 parts
+    half_length = (len(complete_table['Binary representation'][0]) // 2) + 1
+    complete_table['Binary_part0'] = complete_table['Binary representation'].apply(lambda x: x[:half_length]).apply(lambda x: int(x, 2))
+    complete_table['Binary_part1'] = complete_table['Binary representation'].apply(lambda x: x[half_length:]).apply(lambda x: int(x, 2))
+    
+    X = complete_table['Binary_part0']
+    y = complete_table['Binary_part1']
+    
+    # Color gradient according to the score
+    color_gradient = complete_table['Lookup value'].apply(lambda x: (x, 0.1, 0.1))
+    
+    plt.scatter(X, y)
+    plt.title(f"HBM of {dataset_name}")
+    plt.xlabel("Binary representation part 1")
+    plt.ylabel("Binary representation part 2")
+    plt.grid()
+    plt.savefig(f"{dataset_name}_hinged_bitstring.png")
+    plt.show()
+    return"""
+    
+    
+def hamming_distance(x, y):
+    assert len(x) == len(y), "Strings must be of the same length"
+    return sum(el1 != el2 for el1, el2 in zip(x, y))
+
     
 if __name__ == "__main__":
-    print(to_binary_representation((1, 2), 5))
+    
+    # Load complete table csv
+    
+    glass_complete_table = pd.read_csv("glass_complete_table.csv", dtype={'Binary representation': str})
+    wine_complete_table = pd.read_csv("wine_complete_table.csv", dtype={'Binary representation': str})
+    magic_complete_table = pd.read_csv("magic_complete_table.csv", dtype={'Binary representation': str})
+    
+    print(glass_complete_table)
+    visualization_2d(glass_complete_table, "glass")
+    visualization_2d(wine_complete_table, "wine")
+    visualization_2d(magic_complete_table, "magic")
+    
